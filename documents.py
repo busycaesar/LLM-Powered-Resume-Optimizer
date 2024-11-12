87906326ba956c9ba0e3297f6fc04134a313f649
Dev Shah <busycaesar@gmail.com>
2024-11-11 21:24:51 -0500

    Add: Python scripts for operations

---
+++ documents.py
@@ -0,0 +1,31 @@
+from docx import Document
+
+# File Paths.
+job_description_file_path = "./job_description.txt"
+resume_file_path = "./resume.docx"
+projects_file_path = "./projects.docx"
+
+# Get job description content.
+def get_job_description():
+    with open(job_description_file_path,"r") as file:
+        job_description = file.read()
+    
+    return job_description
+
+# Get resume content.
+def get_resume():
+    # Takes the resume
+    doc = Document(resume_file_path)
+    # Extract text from each paragraph and join it into one string
+    resume = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+
+    return resume
+
+# Get list of projects.
+def get_projects():
+    # Takes the document
+    doc = Document(projects_file_path)
+    # Extract text from each paragraph and join it into one string
+    projects = "\n".join([paragraph.text for paragraph in doc.paragraphs])
+
+    return projects
---
+++ get_relevant_projects.py
@@ -0,0 +1,28 @@
+from documents import get_job_description, get_projects
+from langchain.prompts import PromptTemplate
+import generate_response as gen_resp
+
+# Get the job description and list of projects
+job_description = get_job_description()
+projects = get_projects()
+
+# Create a prompt template
+prompt = PromptTemplate(
+    input_variables=['job_description', 'projects'],
+    template="""
+    Given the following job posting and ATS keywords, along with my list of existing projects, can you provide a curated list of 3 projects most relevant to this role? I’d like the selection to highlight projects that align well with the required skills, experience, and job responsibilities described. Return me the list of project in the order of relevance along with the updated, maximum 2-3 line, description which increases the chances that ATS passes my resume. All the sentences of the description should be in active voice and starting with action word.
+
+    Job Posting: {job_description}
+
+    List of Projects: {projects}
+    """
+    )
+
+# Call the function to generate the content
+relevant_projects = gen_resp.generate_response(
+    prompt=prompt, 
+    input_variables={'job_description': job_description, 'projects': projects}
+    )
+
+# Print the response
+print(relevant_projects)
